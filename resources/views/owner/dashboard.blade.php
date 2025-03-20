038e9811c338982d5ee3238267ee1832d5ce3cd0
unknown <hardy.aranzanso.5@gmail.com>
2025-03-20 10:09:07 +0800

    dashboard

--- resources/views/owner/dashboard.blade.php
+++ resources/views/owner/dashboard.blade.php
@@ -3,9 +3,20 @@
 @section('content')
 <x-owner-layout>
   <div class="py-6">
-   jkashdjkasdhsakj
+    <div class="min-w-full mx-auto">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="flex flex-col">
+          <div class="flex items-center gap-4 mb-4 p-2 bg-gray-50 rounded-lg shadow-sm">
+            <h1 class="text-2xl font-semibold text-black">Admin Dashboard</h1>
+          </div>
 
-         
+          @livewire('owner-dashboard') 
+
+        
+        
+        </div>
+      </div>
+    </div>
   </div>
 
 
